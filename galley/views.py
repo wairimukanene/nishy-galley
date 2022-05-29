f4ca221b9defa7559bc5f7a8b9d16d611c5a04ec
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-29 18:54:11 +0300

    commit28-added the function and option to add a new category that is not included in the database

--- galley/views.py
+++ galley/views.py
@@ -21,6 +21,19 @@ def viewphoto(request, pk):
 
 def addphoto(request):
   categories = Category.objects.all()
+  if request.method =='POST':
+    data = request.POST
+    image = request.FILES.get('image')
+    
+    if data['category'] != 'none':
+      category =Category.objects.get(id=data['category'])
+    elif data['category_new'] != '':
+      category, created = Category.objects.get_or_create(name=data['category_new'])
+    else:
+      category= None
+  
+    
+    
   
   context = {'categories':categories}
   
