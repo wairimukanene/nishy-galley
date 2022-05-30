4b6b9a010b2628e1eff6dfa5197a77cc63da2fe3
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-30 14:28:26 +0300

    commit32-added function to filter category and photo

--- galley/models.py
+++ galley/models.py
@@ -13,6 +13,27 @@ class Category(models.Model):
     
   def delete_name(self):
     self.delete()
+    
+    class Location(models.model):
+      location=models.CharField(max_length=100)
+      def __str__(self):
+        return self.location
+      
+      
+  @classmethod
+  def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+  @classmethod
+  def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(photo=value)
+
+  def save_location(self):
+        self.save()
+
+  def delete_location(self):
+        self.delete()
   
   
 class Photo(models.Model):
@@ -20,6 +41,7 @@ class Photo(models.Model):
   Category, on_delete=models.SET_NULL, null=True, blank=True)
   image= models.ImageField(null=False, blank=False)
   description = models.TextField()
+  location = models.ForeignKey(Location,on_delete=models.SET_NULL, null=True, blank=True)
   
   def __str__(self):
     return self.description
--- galley/views.py
+++ galley/views.py
@@ -7,8 +7,13 @@ from .models import Category, Photo
 
 # Create your views here.
 def galley(request):
+  category = request.GET.get('category')
+  if category == None:
+    photos= Photo.objects.all()
+  else:
+    photos = Photo.objects.filter(category__name=category)
+    
   categories = Category.objects.all()
-  photos= Photo.objects.all()
   context = {'categories':categories ,'photos':photos}
   
   return render(request, 'galley/galley.html', context)
